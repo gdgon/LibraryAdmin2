b7e32ea90bbed03eb3ac4ead2f011041a3f2a227
gdgon <g@gdgon.com>
2014-07-27 00:18:45 +0800

    Add untracked fies

---
+++ LibraryAdmin2/Models/Borrower.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LibraryAdmin2.Models
+{
+    public class Borrower
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Required]
+        [Display(Name="First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+        [Display(Name ="Guardian's Phone Number")]
+        public string GuardianPhone { get; set; }
+
+        public static bool Create(Borrower borrower, LibraryAdmin2Db db)
+        {
+            borrower.Name = borrower.FirstName + " " + borrower.LastName;
+            db.Borrowers.Add(borrower);
+            db.SaveChanges();
+            return true;
+        }
+
+        public static bool Edit(Borrower borrower, LibraryAdmin2Db db)
+        {
+            db.Entry(borrower).State = EntityState.Modified;
+            db.SaveChanges();
+            return true;
+        }
+
+        public static bool Delete(Borrower borrower, LibraryAdmin2Db db)
+        {
+            db.Borrowers.Remove(borrower);
+            db.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ LibraryAdmin2/Utils/UrlBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace LibraryAdmin2.Utils
+{
+    public class UrlBuilder
+    {
+        public UrlBuilder(Controller context, string action, string controller)
+        {
+            url.Append(context.Url.Action(action, controller, null, context.Request.Url.Scheme) + "?");
+        }
+
+        private StringBuilder url = new StringBuilder();
+
+        public void AppendParam(string key, object value)
+        {
+            url.Append(key);
+            url.Append("=");
+            url.Append(HttpContext.Current.Server.UrlEncode(value.ToString()));
+            url.Append("&");
+        }
+
+        public override string ToString()
+        {
+ 	         return url.ToString();
+        }
+    }
+}
