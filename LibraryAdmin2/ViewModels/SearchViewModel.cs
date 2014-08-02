6bc3e2cb144d18320b8f6b7d07cd1719ab9185c2
gdgon <g@gdgon.com>
2014-08-02 22:39:42 +0800

    Added SearchViewModel

---
+++ LibraryAdmin2/ViewModels/SearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryAdmin2.ViewModels
+{
+    public class SearchViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string SearchType { get; set; }
+        public bool? Partial { get; set; }
+    }
+}
