864c3dfa29940134868ac13f9a7809d0a7518659
gdgon <g@gdgon.com>
2014-08-03 20:55:30 +0800

    Featured items: Book, Borrower

---
+++ LibraryAdmin2/Controllers/FeaturedController.cs
@@ -0,0 +1,49 @@
+﻿using LibraryAdmin2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryAdmin2.Controllers
+{
+    public class FeaturedController : Controller
+    {
+        private LibraryAdmin2Db db = new LibraryAdmin2Db();
+
+        public ActionResult TopBorrower()
+        {
+            var topGroup = db.LogEvents.Where(e => e.Event == LogEvent.EventTypes.RequestApproved)
+                                 .GroupBy(e => e.BorrowerId)
+                                 .OrderByDescending(e => e.Count())
+                                 .First();
+            var id = topGroup.First().BorrowerId;
+
+            ViewBag.Name = db.Borrowers.Find(id).Name;
+            ViewBag.Num = topGroup.Count();
+            return PartialView();
+        }
+
+        public ActionResult TopBook()
+        {
+            var topGroup = db.LogEvents.Where(e => e.Event == LogEvent.EventTypes.RequestApproved)
+                                 .GroupBy(e => e.BookId)
+                                 .OrderByDescending(e => e.Count())
+                                 .First();
+            var id = topGroup.First().BorrowerId;
+
+            var topBook = db.Books.Find(id);
+            ViewBag.Num = topGroup.Count();
+            return PartialView(topBook);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
